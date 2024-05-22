8652d7edc03ac221cca6725e74f66932f9e5f3ea
Ahmed Sheikh <ahmedos099912@gmail.com>
2024-05-22 16:55:59 +0400

    intergrated front with API

--- frontend/src/components/Card.js
+++ frontend/src/components/Card.js
@@ -1,8 +1,11 @@
-const Card = () => {
+import formatDistanceToNow from 'date-fns/formatDistanceToNow'
+
+const Card = ({task}) => {
     return ( <div className="rounded-md bg-gray-900 shadow-md p-4">
-        <div className="flex justify-between"><h2 className=" font-bold">Model Answer</h2><p className="text-xs">2</p></div>
+        <div className="flex justify-between"><h2 className=" font-bold">{task.title}</h2><p className="text-xs">{formatDistanceToNow(new Date(task.createdAt),{addSuffix:true})}</p></div>
+        <p className='my-4 text-sm text-gray-400'>This is a descripation of the task</p>
         <div className="flex my-2">
-            <div className="w-1/4 rounded bg-red-950 mr-2"><p className=" text-red-500 text-xs text-center">TODO</p></div>
+            <div className="w-1/4 rounded bg-red-950 mr-2"><p className=" text-red-500 text-xs text-center">{task.category}</p></div>
             <div className="w-1/4 rounded bg-blue-950 mr-2"><p className=" text-blue-500 text-xs text-center">Design</p></div>
             <div className="w-1/4 rounded bg-green-950 mr-2"><p className=" text-green-500 text-xs text-center">Backlog</p></div>
         </div>
--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -1,7 +1,26 @@
+import { useEffect, useState } from "react";
 import Card from "./Card";
 import { IoIosAddCircle } from "react-icons/io";
 
+
 const Dashboard = () => {
+
+    const [tasks,setTasks] = useState([])
+
+    useEffect(()=>{
+        const fetchTasks = async()=>{
+            const response = await fetch('/api/tasks')
+            const json = await response.json()
+
+            if(response.ok){
+                setTasks(json)
+                console.log(json)
+            }
+        }
+        fetchTasks()
+        
+    },[])
+
     return ( <div className="p-2 ">
         
         <input type="text" className="w-full m-4 rounded-full p-2 bg-slate-400 text-gray-100 placeholder:text-gray-100 border-gray-600 border-4" placeholder="Search here ..."/>
@@ -10,16 +29,11 @@ const Dashboard = () => {
         <h4 className=" text-lg font-semibold">Facebook</h4>
 
         <div className="grid grid-cols-4 gap-2 custom-scrollbar auto-rows-min">
-            <Card></Card>
-            <Card></Card>
-            <Card></Card>
-            <Card></Card>
-            <Card></Card>
-            <Card></Card>
-            <Card></Card>
-            <Card></Card>
-            <Card></Card>
-            <Card></Card>
+            {
+                tasks && tasks.map((task)=>(
+                   <div key={task._id}><Card task={task}></Card></div> 
+                ))
+            }
             
             <div className="rounded-md bg-gray-900 hover:bg-slate-800 shadow-md p-4 cursor-pointer flex justify-center items-center">
             <div className="h-16 w-16 rounded-full bg-slate-600 flex justify-center items-center"><IoIosAddCircle className="text-3xl"/></div>
