1890ba0a0daf9fa92b9afa6af626d7deb12ca43d
Ahmed Sheikh <ahmedos099912@gmail.com>
2024-05-28 15:45:42 +0400

    register task delete delete

--- backend/routes/taskRoutes.js
+++ backend/routes/taskRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const {createTask,getTasks} = require('../controllers/taskController')
+const {createTask,getTasks,deleteTask} = require('../controllers/taskController')
 
 const router = express.Router()
 
@@ -7,6 +7,8 @@ const router = express.Router()
 router.get('/',getTasks)
 
 router.post('/',createTask)
+
+router.delete('/:id',deleteTask)
 
 
 module.exports = router
