6cbc913517ba487654a21a6535f65b483b8261a7
Ahmed Sheikh <ahmedos099912@gmail.com>
2024-05-28 15:43:43 +0400

    delete task controller

--- backend/controllers/taskController.js
+++ backend/controllers/taskController.js
@@ -1,6 +1,6 @@
 const Task = require('../models/taskModel')
 
-// const mongoose =  require('mongoose')
+const mongoose =  require('mongoose')
 
 
 const getTasks = async(req,res)=>{
@@ -25,7 +25,25 @@ const createTask = async(req,res)=>{
  }
 }
 
+const deleteTask = async (req,res)=>{
+   const {id} = req.params
+   
+   if(!mongoose.Types.ObjectId.isValid(id)){
+      return res.status(404).json({error:"No such Task"})
+   }
+
+   const task = await Task.findOneAndDelete({_id:id})
+
+   if(!task){
+      return res.status(400).json({error:'No such task'})
+   }
+
+   res.status(200).json(task)
+
+}
+
 module.exports = {
     createTask,
-    getTasks
+    getTasks,
+    deleteTask
 }
