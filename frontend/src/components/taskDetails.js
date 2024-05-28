d7e05aef57d81b20040a4573148087bf02204512
Ahmed Sheikh <ahmedos099912@gmail.com>
2024-05-28 16:36:20 +0400

    add delete to button

--- frontend/src/components/taskDetails.js
+++ frontend/src/components/taskDetails.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { MdDelete } from "react-icons/md";
 import Modal from "react-modal";
 
 const customStyles = {
@@ -16,23 +17,40 @@ const customStyles = {
 
 const TaskDetails = (task) => {
 
-    const [modalOpen,setModalOpen] = useState(true)
+  const [modalOpen,setModalOpen] = useState(true)
+  const [error,setError] = useState()
+
+  const deletetask = async()=>{
+    const response = await fetch('api/tasks/'+task.task._id,{
+      method:'DELETE',
+    })
+    const json =  await response.json()
+
+    if(!response.ok){
+      setError(json.error)
+    }
+    setModalOpen(false)
+  }
+
+    
 
     useEffect(()=>{
         setModalOpen(true)
-    },[task])
+    },[])
 
-    return ( task && <div>
+    return (<div>
         <Modal
         isOpen={modalOpen}
-        onRequestClose={setModalOpen(false)}
+        onRequestClose={()=>setModalOpen(false)}
         style={customStyles}
         appElement={document.getElementById('root')}
       >
-        <h2>{task.task.title}</h2>
+        <div className="flex justify-between mx-2"><h2>{task.task.title}</h2><MdDelete onClick={deletetask}/></div>
         <div>{task.task.createdAt}</div>
         <p>{task.task.description}</p>
         <p>{task.task.category}</p>
+        {error && <div>{error}</div>}
+        <h2>test</h2>
       </Modal>
     </div> );
 }
