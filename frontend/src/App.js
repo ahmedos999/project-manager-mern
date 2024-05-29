abe561d0d1842426f9eed2710539a5ef604c5646
Ahmed Sheikh <ahmedos099912@gmail.com>
2024-05-29 15:43:27 +0400

    added react router and login pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,21 @@
 import Home from "./pages/Home";
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
 function App() {
   return (
     <div className="App">
-      <Home></Home>
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
+      </Routes>
+      <Routes>
+        <Route path="/login" element={<Login/>}></Route>
+      </Routes>
+      <Routes>
+        <Route path="/signup" element={<Signup/>}></Route>
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ frontend/src/pages/Login.js
@@ -0,0 +1,17 @@
+const Login = () => {
+    return ( <div className="flex flex-col w-2/6 bg-slate-600 rounded justify-around p-6 mx-auto my-40">
+        <h2 className=" text-2xl font-bold teko">Welcome to Project manager</h2>
+        <div className="flex flex-col my-4">
+        <label htmlFor="" className="">Email</label>
+        <input type="text" name="" id="" className="rounded border-2 border-slate-800 text-slate-900 p-1"/>
+        </div>
+        <div className="flex flex-col my-4">
+        <label htmlFor="">Password</label>
+        <input type="password" name="" id="" className="rounded border-2 border-slate-800 text-slate-900 p-1"/>
+        </div>
+        <a href="/signup" className="text-sm underline">Dont have an account signup?</a>
+        <button className="bg-slate-800 w-fit py-2 px-8 rounded mx-auto mt-4">Login</button>
+    </div> );
+}
+ 
+export default Login;
