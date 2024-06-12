d30374a05e5c9190c888d1c0fef9fa60f8dbd532
Ahmed Sheikh <ahmedos099912@gmail.com>
2024-06-12 15:44:15 +0400

    add logout

--- frontend/src/components/sidebar.js
+++ frontend/src/components/sidebar.js
@@ -1,7 +1,13 @@
+import { useAuthContext } from '../hooks/useAuthContext';
 import img from '../imgs/pp.jpg'
 
 
 const SideBar = () => {
+    const {dispatch} = useAuthContext()
+    const logout =()=>{
+        localStorage.removeItem('user')
+        dispatch({type:'LOGOUT'})
+    }
     return ( <div className="sidebar p-4 flex flex-col h-screen w-full justify-between">
         <div className="flex items-center mt-8">
             <div className="w-14 h-14 mr-2"><img src={img} alt="" className='rounded-full'/></div>
@@ -14,9 +20,9 @@ const SideBar = () => {
             <li>Threads</li>
             <li>Reddit</li>
         </ul>
-        <div>
-            logout
-        </div>
+        <button onClick={logout} className='font-bold self-start rounded p-1 bg-gray-800'>
+            Logout
+        </button>
     </div> );
 }
  
