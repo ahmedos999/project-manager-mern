fa63d15a3e661594da142bebfb9d2f88e543a34d
Ahmed Sheikh <ahmedos099912@gmail.com>
2024-06-20 16:50:43 +0400

    edit task details

--- frontend/src/components/taskDetails.js
+++ frontend/src/components/taskDetails.js
@@ -3,6 +3,7 @@ import { MdDelete } from "react-icons/md";
 import Modal from "react-modal";
 import { useTaskContext } from "../hooks/useTaskContext";
 import {useAuthContext} from '../hooks/useAuthContext'
+import formatDistanceToNow from 'date-fns/formatDistanceToNow'
 
 const customStyles = {
     content: {
@@ -53,12 +54,30 @@ const TaskDetails = (task) => {
         style={customStyles}
         appElement={document.getElementById('root')}
       >
-        <div className="flex justify-between mx-2"><h2>{task.task.title}</h2><MdDelete onClick={deletetask}/></div>
-        <div>{task.task.createdAt}</div>
-        <p>{task.task.description}</p>
-        <p>{task.task.category}</p>
+        <div className="flex justify-between items-center"><div className="flex"><h2 className=" font-bold text-xl">{task.task.title}</h2></div ><div className=" rounded-full bg-slate-600 p-1"><MdDelete className="text-red-500 text-xl" onClick={deletetask}/></div></div>
+        
+        <div className="my-2">
+        <h2 className=" text-slate-400 ">Description</h2>
+        <p className="ml-2 ">{task.task.description}</p>
+        </div>
+
+        <div>
+          <h2 className=" text-slate-400">Categories</h2>
+        <p className=" bg-red-600 rounded-full py-1 text-xs px-2 w-min text-red-200">{task.task.category}</p>
+        </div>
+
+        <div className="my-2">
+          <h2 className=" text-slate-400 ">Task participants</h2>
+          <div className="flex gap-1 flex-wrap text-slate-200">{task.task.participants.map((e)=>(<div className=" bg-slate-500 py-1 px-2 rounded-full text-xs">{e}</div>))}</div>
+        </div>
+
+        <div className="my-2">
+          <h2 className="text-slate-400">Created by</h2>
+          <p className="text-sm">Me@projectmanger.com</p>
+        </div>
+
+        <div className="flex justify-end text-sm text-slate-400">{} {formatDistanceToNow(new Date(task.task.createdAt),{addSuffix:true})}</div>
         {error && <div>{error}</div>}
-        <h2>test</h2>
       </Modal>
     </div> );
 }
