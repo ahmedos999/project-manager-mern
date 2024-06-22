bc4b6fbeff7a2559b4d21205c94e06c1f5871046
Ahmed Sheikh <ahmedos099912@gmail.com>
2024-06-22 19:40:04 +0400

    set name an icon for user profile

--- frontend/src/components/sidebar.js
+++ frontend/src/components/sidebar.js
@@ -3,15 +3,15 @@ import img from '../imgs/pp.jpg'
 
 
 const SideBar = () => {
-    const {dispatch} = useAuthContext()
+    const {user,dispatch} = useAuthContext()
     const logout =()=>{
         localStorage.removeItem('user')
         dispatch({type:'LOGOUT'})
     }
     return ( <div className="sidebar p-4 flex flex-col h-screen w-full justify-between">
         <div className="flex items-center mt-8">
-            <div className="w-14 h-14 mr-2"><img src={img} alt="" className='rounded-full'/></div>
-            <h2 className="">Ahmed Sheikh</h2>
+            <div className="w-14 h-14 mr-2 bg-slate-600 rounded-full flex justify-center items-center text-3xl">{user.email.substring(0,1).toUpperCase()}</div>
+            <h2 className="">{user.email.substring(0,(user.email.indexOf('@')))}</h2>
         </div>
         <button onClick={logout} className='font-bold self-start rounded p-1 bg-gray-800'>
             Logout
