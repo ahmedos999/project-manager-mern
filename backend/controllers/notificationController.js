f7001ea39adfeff28601299e34960ada609cbb6b
Ahmed Sheikh <ahmedos099912@gmail.com>
2024-07-04 20:43:15 +0400

    sort notification

--- backend/controllers/notificationController.js
+++ backend/controllers/notificationController.js
@@ -5,7 +5,7 @@ const getAllUserNotification = async(req,res)=>{
 
 
     try {
-        const notifications = await Notification.find({ user_id });
+        const notifications = await Notification.find({ user_id }).sort({createdAt:-1});
         res.status(200).json(notifications);
       } catch (error) {
         res.status(400).json({ error: error.message });
