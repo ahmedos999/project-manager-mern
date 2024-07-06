90c1f483115ad42c7f978ab983b23f962e0f17f2
Ahmed Sheikh <ahmedos099912@gmail.com>
2024-07-06 20:26:48 +0400

    filter category V1

--- frontend/src/components/sidebar.js
+++ frontend/src/components/sidebar.js
@@ -8,12 +8,29 @@ const SideBar = () => {
     const {tasks,dispatch:dispatchTask} = useTaskContext()
     const [categories,setCategories] = useState([])
     const [flag,setFlag] = useState(true)
+    const [allTasks,setAllTasks] = useState([])
+    const [filterList, setFilterList] = useState([]);
     
     
 
-    const filterTasks =(categoryFilter)=>{
+
+    const addFilter = (filter)=>{
+        if(filterList.includes(filter)){
+            const index = filterList.indexOf(filter)
+            filterList.splice(index,1)
+            setFilterList([...filterList])
+            filterTasks(filterList)
+            return
+        }
+        setFilterList(prevFilterList => [...prevFilterList, filter]);
+        filterTasks([...filterList, filter]);
+    }
+    const filterTasks =(updatedFilterList)=>{
+        console.log(updatedFilterList)
+        // console.log(allTasks)
         setFlag(false)
-        const filter = tasks.filter((task)=>task.category===categoryFilter)
+        var filter = allTasks.filter((task)=>updatedFilterList.includes(task.category))
+        if(filter.length===0) filter = allTasks
         dispatchTask({type:'SET_TASKS',payload:filter})
     }
     useEffect(()=>{
@@ -24,6 +41,7 @@ const SideBar = () => {
                 if(!uniqueTags.includes(element.category)) uniqueTags.push(element.category)
             });
             setCategories(uniqueTags)
+            setAllTasks(tasks)
         }
         
         
@@ -32,6 +50,7 @@ const SideBar = () => {
     const logout =()=>{
         localStorage.removeItem('user')
         setCategories(null)
+        setFilterList(null)
         dispatch({type:'LOGOUT'})
     }
     return ( <div className="sidebar p-4 flex flex-col h-screen w-full justify-between">
@@ -39,8 +58,8 @@ const SideBar = () => {
             <div className="w-14 h-14 mr-2 bg-slate-600 rounded-full flex justify-center items-center text-3xl">{user.email.substring(0,1).toUpperCase()}</div>
             <h2 className="">{user.email.substring(0,(user.email.indexOf('@')))}</h2>
         </div>
-        <div className='grid grid-cols-2'>
-        {categories && categories.map((category,index)=>(<div className='my-1' key={index} onClick={()=>filterTasks(category)} > <p className='p-1 bg-slate-500 rounded-full w-min'>{category}</p></div>))}
+        <div className='flex gap-1 flex-wrap'>
+        {categories && categories.map((category,index)=>(<div className='my-1' key={index} onClick={()=>addFilter(category)} > <p className={`p-1 rounded-full w-min text-sm${filterList.includes(category)?` bg-red-400`:` bg-slate-500`}`}>{category}</p></div>))}
         </div>
         <button onClick={logout} className='font-bold self-start rounded p-1 bg-gray-800'>
             Logout
