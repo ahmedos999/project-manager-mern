60520eaa410de1e4e1c6ca6a3a91a31ba65843f5
Ahmed Sheikh <ahmedos099912@gmail.com>
2024-07-11 22:13:51 +0400

    change friendlist display

--- frontend/src/components/FriendCard.js
+++ frontend/src/components/FriendCard.js
@@ -1,8 +1,25 @@
 
 
-const FriendCard = ({img}) => {
-    return ( <div className="w-14  h-14 ">
-        <img src={img} alt="" className='rounded-full inherit'/>
+const FriendCard = ({name}) => {
+    const color = {
+        0:"bg-cyan-500",
+        1:"bg-red-500",
+        2:"bg-green-500",
+        3:"bg-blue-500",
+        4:"bg-purple-500",
+        5:"bg-yellow-500",
+
+    }
+
+    let number = Math.floor(Math.random() * 6)
+
+    console.log(number)
+    return ( 
+<div className="email-container">
+      <div className={`email-initial ${color[number]}`}>
+        {name.email[0].toUpperCase()}
+        <div className={`email-full ${color[number]}`}>{name.email}</div>
+      </div>
     </div> );
 }
  
--- frontend/src/components/FriendsList.js
+++ frontend/src/components/FriendsList.js
@@ -1,17 +1,31 @@
 import FriendCard from "./FriendCard";
-import img from '../imgs/f1.jpg'
-import img1 from '../imgs/f2.jpg'
-import img2 from '../imgs/f3.jpg'
-import img3 from '../imgs/f4.jpg'
-import img4 from '../imgs/f5.jpg'
+import {useEffect, useState} from 'react'
+import { useAuthContext } from "../hooks/useAuthContext";
 
 const Friendslist = () => {
-    return ( <div className="flex flex-col gap-4">
-        <FriendCard img={img}></FriendCard>
-        <FriendCard img={img1}></FriendCard>
-        <FriendCard img={img2}></FriendCard>
-        <FriendCard img={img3}></FriendCard>
-        <FriendCard img={img4}></FriendCard>
+    const [friends,setFriends] = useState([])
+    const {user} = useAuthContext()
+
+    useEffect(()=>{
+
+        const fetchfriends = async()=>{
+            const res = await fetch('/api/user/allusers')
+
+            const json = await res.json()
+
+            if(res.ok){
+                let temp = json.filter(e=>e.email!==user.email)
+                setFriends(temp)
+                
+            }
+        }
+        fetchfriends()
+    },[])
+    return ( <div className="flex flex-col gap-2">
+        {
+            friends && friends.map((friend)=>(<FriendCard name={friend}/>))
+        }
+        
     </div> );
 }
  
