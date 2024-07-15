79119f6e6af466efc19d858aa2f0ac9d98b586d3
Ahmed Sheikh <ahmedos099912@gmail.com>
2024-07-15 18:57:45 +0400

    login page v-1

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -9,20 +9,22 @@ const Login = () => {
     const submit = async()=>{
         await signin(email,password)
     }
-    return ( <div className="flex flex-col w-2/6 bg-slate-600 rounded justify-around p-6 mx-auto my-40">
-        <h2 className=" text-2xl font-bold teko">Welcome to Project manager</h2>
-        <div className="flex flex-col my-4">
+    return ( 
+        <div className="w-screen h-screen flex justify-center items-center bg-img">
+            <div className="flex flex-col w-4/12 h-3/5 bg-slate-800 rounded-lg justify-between p-6 mx-auto my-40">
+        <h2 className=" text-2xl font-bold teko text-center">Welcome to <span className="bg-slate-200 p-1 text-black rounded">Project manager</span></h2>
+        <div className="flex flex-col">
         <label htmlFor="" className="">Email</label>
-        <input type="text" name="" id="email" className="rounded border-2 border-slate-800 text-slate-900 p-1" value={email} onChange={(e)=>setEmail(e.target.value)}/>
-        </div>
-        <div className="flex flex-col my-4">
+        <input type="text" name="" placeholder="example@mail.com" id="email" className="mb-2 rounded border-2 border-slate-800 text-slate-900 p-1" value={email} onChange={(e)=>setEmail(e.target.value)}/>
         <label htmlFor="">Password</label>
-        <input type="password" name="" id="pass" className="rounded border-2 border-slate-800 text-slate-900 p-1" value={password} onChange={(e)=>setPassowrd(e.target.value)}/>
-        </div>
+        <input type="password" placeholder="xxxxxxx" name="" id="pass" className="mb-2 rounded border-2 border-slate-800 text-slate-900 p-1" value={password} onChange={(e)=>setPassowrd(e.target.value)}/>
         <a href="/signup" className="text-sm underline">Dont have an account signup?</a>
-        <button className="bg-slate-800 w-fit py-2 px-8 rounded mx-auto mt-4" onClick={submit} disabled={isLoading}>Login</button>
+        </div>
+        <button className="bg-slate-600 w-full py-2 px-8 rounded mx-auto mt-4 text-white font-bold hover:bg-slate-200 hover:text-black transition-all" onClick={submit} disabled={isLoading}>Login</button>
         {error && <div className='mt-2 text-sm text-red-500 '>{error}</div>}
-    </div> );
+    </div>
+        </div>
+);
 }
  
 export default Login;
--- frontend/src/pages/Signup.js
+++ frontend/src/pages/Signup.js
@@ -3,29 +3,37 @@ import {useState} from 'react'
 import { useSignup } from '../hooks/useSignup'
 
 export default function Signup() {
-    const [email,setEmai] = useState()
+    const [email,setEmail] = useState()
     const [password,setPassword] = useState()
-    // const [error,setError] = useState()
+    const [rePassword,setRePassword] = useState()
+    const [passError,setPassError] = useState()
     const {signup,isLoading,error} = useSignup()
 
 
 
     const submit = async()=>{
+        if(password !== rePassword){
+            setPassError('Password mismatch')
+            return
+        }
         await signup(email,password)
     }
 
-    return ( <div className="flex flex-col w-2/6 bg-slate-600 rounded justify-around p-6 mx-auto my-40">
-    <h2 className=" text-2xl font-bold teko">Welcome to Project manager</h2>
-    <div className="flex flex-col my-4">
+    return ( <div className="w-screen h-screen flex justify-center items-center bg-img">
+        <div className="flex flex-col w-4/12 h-3/5 bg-slate-800 rounded-lg justify-between p-6 mx-auto my-40">
+    <h2 className=" text-2xl font-bold teko text-center">Welcome to <span className="bg-slate-200 p-1 text-black rounded">Project manager</span></h2>
+    <div className="flex flex-col">
     <label htmlFor="" className="">Email</label>
-    <input type="text"  className="rounded border-2 border-slate-800 text-slate-900 p-1" onChange={(e)=>setEmai(e.target.value)}/>
-    </div>
-    <div className="flex flex-col my-4">
+    <input type="text" name="" placeholder="example@mail.com" id="email" className="mb-2 rounded border-2 border-slate-800 text-slate-900 p-1" value={email} onChange={(e)=>setEmail(e.target.value)}/>
     <label htmlFor="">Password</label>
-    <input type="password" className="rounded border-2 border-slate-800 text-slate-900 p-1" onChange={(e)=>setPassword(e.target.value)}/>
+    <input type="password" placeholder="xxxxxxx" name="" id="pass" className="mb-2 rounded border-2 border-slate-800 text-slate-900 p-1" value={password} onChange={(e)=>setPassword(e.target.value)}/>
+    <label htmlFor="">Re-enter Password</label>
+    <input type="password" placeholder="xxxxxxx" name="" id="pass" className="mb-2 rounded border-2 border-slate-800 text-slate-900 p-1" value={rePassword} onChange={(e)=>setRePassword(e.target.value)}/>
+    {passError && <div className='mt-2 text-sm text-red-500 '>{passError}</div>}
+    <a href="/login" className="text-sm underline">Already have an account ?</a>
     </div>
-    <a href="/login" className="text-sm underline">Already have an account signup?</a>
-    <button className="bg-slate-800 w-fit py-2 px-8 rounded mx-auto mt-4" onClick={submit} disabled={isLoading}>Sign Up</button>
+    <button className="bg-slate-600 w-full py-2 px-8 rounded mx-auto mt-4 text-white font-bold hover:bg-slate-200 hover:text-black transition-all" onClick={submit} disabled={isLoading}>Sign up</button>
     {error && <div className='mt-2 text-sm text-red-500 '>{error}</div>}
-</div> );
+</div>
+    </div> );
 }
