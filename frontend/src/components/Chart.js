940794133adbec8e15b80575ba3261400c8c5c7c
Ahmed Sheikh <ahmedos099912@gmail.com>
2024-07-31 23:55:20 +0400

    fix the chart bug

--- frontend/src/components/Chart.js
+++ frontend/src/components/Chart.js
@@ -9,27 +9,36 @@ import { useEffect, useState } from 'react';
   const COLORS = ['#f87171', '#facc15', '#60a5fa', '#4ade80'];
 export const Chart = ()=>{
     const [data,setData] = useState([
-        { name: 'Group A', value: 0 },
-        { name: 'Group B', value: 0 },
-        { name: 'Group C', value: 0 },
-        { name: 'Group D', value: 0 },
+        { name: 'urgent', value: 0 },
+        { name: 'progress', value: 0 },
+        { name: 'long', value: 0 },
+        { name: 'done', value: 0 },
       ])
 
     const {tasks} = useTaskContext()
     useEffect(()=>{
-        if(tasks && tasks.length!==0){
-            tasks.forEach(task => {
-                switch(task.status){
-                    case 'urgent':data[0].value++;break;
-                    case 'progress':data[1].value++;break;
-                    case 'long':data[2].value++;break;
-                    case 'done':data[3].value++;break;
-                    default:return
-                }
-            });
-        }
-        setData([...data])
-    },[data,tasks])
+      console.log('ran')
+      const newData = [
+        { name: 'urgent', value: 0 },
+        { name: 'progress', value: 0 },
+        { name: 'long', value: 0 },
+        { name: 'done', value: 0 },
+      ];
+  
+      if (tasks && tasks.length !== 0) {
+        tasks.forEach(task => {
+          switch (task.status) {
+            case 'urgent': newData[0].value++; break;
+            case 'progress': newData[1].value++; break;
+            case 'long': newData[2].value++; break;
+            case 'done': newData[3].value++; break;
+            default: return;
+          }
+        });
+      }
+  
+      setData(newData); 
+    },[tasks])
     return(<div> 
         {tasks && tasks.length > 0 && <PieChart width={110} height={110} >
         <Pie
